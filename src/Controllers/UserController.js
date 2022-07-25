a35b477dc4f5b1cd3fb0fd6bf3b4254bbcb9596c
Vander-Reis <vanderreis2017@outlook.com>
2022-07-25 11:35:03 -0300

    adição de update de usuario

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -32,6 +32,35 @@ class UserController {
 
    }
 
+   async update(request, response) {
+
+    const { name, email, password, oldPassword } = request.body;
+    const { id } = request.params; 
+
+    // conexão com o banco
+
+    const database = await sqliteConnection();
+
+    // verificar se o usuario existe
+
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+
+    if(!user) {
+        throw new AppError("Usuário não encontrado");
+    }
+
+    // verifica se o e-mail informado já está em uso
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)",[email]);
+
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+        throw new AppError("Esté e-mail já está em uso.");
+    }
+
+
+
+    
+   }
+
 }
 
 module.exports = UserController;
